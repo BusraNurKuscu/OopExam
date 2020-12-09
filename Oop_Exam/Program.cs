97e9ffc66bc0ed3c93cb3bc7e3cb0b1a97dbf0b9
Büşra Nur Kuşçu <191118034@samsun.edu.tr>
2020-12-09 22:46:53 +0300

    Add project files.

---
+++ Oop_Exam/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+/*
+ CREATE TABLE [dbo].[Urun] (
+    [Id]       INT          IDENTITY (1, 1) NOT NULL,
+    [URUN]     VARCHAR (50) NOT NULL,
+    [FİYAT]    INT          NULL,
+    [STOK]     INT          NULL,
+    [KATAGORİ] VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+ */
+namespace Oop_Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            urunleriTemizle();
+            urunleriEkle();
+            urunleriBas();
+
+            Console.WriteLine("\nSilme");
+            var urun = Urun.GetAll().FirstOrDefault();
+            if (urun != null)
+            {
+                urun.Delete();
+            }
+            urunleriBas();
+
+
+
+
+            Console.WriteLine("\nGüncelleme");
+            urun = Urun.GetAll().FirstOrDefault();
+            if (urun != null)
+            {
+                urun.katagori = "test kategori";
+                urun.InsertOrUpdate();
+            }
+            urunleriBas();
+
+            Console.ReadLine();
+
+        }
+        public static void urunleriEkle()
+        {
+            new Urun("Sampuan", 12, 33, "Temizlik").InsertOrUpdate();
+            new Urun("Islak Havlu", 15, 37, "Temizlik").InsertOrUpdate();
+            new Urun("Cikolata", 5, 25, "Yiyecek").InsertOrUpdate();
+            new Urun("Sut", 3, 317, "Yiyecek").InsertOrUpdate();
+            new Urun("Kitap", 35, 50, "Kırtasiye").InsertOrUpdate();
+            new Urun("Zeytin", 40, 437, "Yiyecek").InsertOrUpdate();
+            new Urun("Cay", 27, 55, "Yiyecek").InsertOrUpdate();
+            new Urun("Krem", 15, 40, "Kozmetik").InsertOrUpdate();
+            new Urun("Sabun", 5, 81, "Temizlik").InsertOrUpdate();
+        }
+        public static void urunleriTemizle()
+        {
+            var uruns = Urun.GetAll();
+            foreach (var item in uruns)
+            {
+                item.Delete();
+            }
+        }
+        public static void urunleriBas()
+        {
+            var urunler = Urun.GetAll();
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun);
+            }
+        }
+    }
+}
---
+++ Oop_Exam/Urun.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Oop_Exam
+{
+    public class Urun
+    {
+        private static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=URUNDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public int id { get; set; }
+        public string urun { get; set; }
+        public int fiyat { get; set; }
+        public int stok { get; set; }
+        public string katagori { get; set; }
+        public Urun()
+        {
+
+        }
+
+        public Urun(int id, string urun, int fiyat, int stok, string katagori)
+        {
+            this.id = id;
+            this.urun = urun;
+            this.fiyat = fiyat;
+            this.stok = stok;
+            this.katagori = katagori;
+        }
+        public Urun( string urun, int fiyat, int stok, string katagori)
+        {
+            this.id = 0;
+            this.urun = urun;
+            this.fiyat = fiyat;
+            this.stok = stok;
+            this.katagori = katagori;
+        }
+        public static Urun GetWithId(int id)
+        {
+            Urun item = null;
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            var komut = new SqlCommand();
+            komut.Connection = con;
+            komut.CommandText = $"Select * from [dbo].[Urun] where ID = {id}";
+            var reader = komut.ExecuteReader();
+            while (reader.Read())
+            {
+                item = new Urun(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetString(4));
+            }
+            con.Close();
+            return item;
+        }
+        public void InsertOrUpdate()
+        {
+           
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            var komut = new SqlCommand();
+            komut.Connection = con;
+            if (id == 0)
+            {
+                komut.CommandText = $"INSERT INTO [dbo].[Urun] ([URUN], [FİYAT], [STOK], [KATAGORİ]) VALUES ('{urun}', {fiyat}, {stok}, '{katagori}');";
+            }
+            else                                                          
+            {
+                komut.CommandText = $@"UPDATE [dbo].[Urun] SET [URUN] = '{urun}', [FİYAT] = {fiyat}, [STOK] = {stok}, [KATAGORİ] = '{katagori}' WHERE ID={id};";
+            }
+            komut.ExecuteNonQuery();
+            con.Close();
+        }
+        public void Delete()
+        {
+            if (id <= 0) return;
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            var komut = new SqlCommand();
+            komut.Connection = con;
+            komut.CommandText = $@"DELETE FROM [dbo].[Urun] WHERE ID={id};";
+            komut.ExecuteNonQuery();
+            con.Close();
+        }
+        public static List<Urun> GetAll()
+        {
+            List<Urun> uruns = new List<Urun>();
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            var komut = new SqlCommand();
+            komut.Connection = con;
+            komut.CommandText = $"Select * from [dbo].[Urun];";
+            var reader = komut.ExecuteReader();
+            while (reader.Read())
+            {
+                var item = new Urun(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetString(4));
+                uruns.Add(item);
+            }
+            con.Close();
+            return uruns;
+        }
+        public override string ToString()
+        {
+            return $"{id}, {urun}, {fiyat}, {stok}, {katagori}";
+        }
+    }
+    
+}
